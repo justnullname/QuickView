0ea8b98f78f47ee1f933d6a9240291ad39aedf78
Vivor Loong <vivoor@live.com>
2025-12-23 12:08:13 +0800

    Cleanup 1.x: Removed QuickView, custom-ports, .github

--- custom-ports/zlib/portfile.cmake
+++
@@ -1,5 +0,0 @@
-# zlib -> zlib-ng redirect overlay port
-# This port redirects zlib dependency to zlib-ng with ZLIB_COMPAT mode
-# zlib-ng provides faster SIMD-optimized compression/decompression
-
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
